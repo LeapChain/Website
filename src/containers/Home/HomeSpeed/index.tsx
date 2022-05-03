8e6d3411e39390944f5f3df61134f9a00ae633c2
jamessspanggg <jamespangmunwai@gmail.com>
2022-05-03 15:11:24 +0800

    fix: remove visibility sensor and count up animation

--- src/containers/Home/HomeSpeed/index.tsx
+++ src/containers/Home/HomeSpeed/index.tsx
@@ -1,7 +1,6 @@
 import React, {FC} from 'react';
 
 import CountUp from 'react-countup';
-import VisibilitySensor from 'react-visibility-sensor';
 
 import {Button} from 'components';
 import {URLS} from 'constants/routes';
@@ -14,12 +13,10 @@ import * as S from './Styles';
 const TRANSACTIONS_PER_SECOND = 3238;
 const LATENCY = 0.015;
 const COUNT_UP_DURATION = 1.5;
-const COUNT_UP_DEFAULT_DURATION = 100000;
 const COUNT_UP_DELAY = 0.3;
 
 const HomeSpeed: FC = () => {
   const {width} = useWindowDimensions();
-  const [viewPortEntered, setViewPortEntered] = React.useState(false);
 
   return (
     <S.Container>
@@ -36,54 +33,44 @@ const HomeSpeed: FC = () => {
           Whitepaper
         </Button>
       )}
-      <VisibilitySensor
-        active={!viewPortEntered}
-        onChange={(isVisible) => {
-          if (isVisible) {
-            setViewPortEntered(true);
-          }
-        }}
-        delayedCall
-      >
-        <S.StatisticsContainer>
-          <S.StatisticSection>
-            <S.StatisticTitle>Speed</S.StatisticTitle>
-            <S.StatisticNumber>
-              <CountUp
-                delay={COUNT_UP_DELAY}
-                duration={viewPortEntered ? COUNT_UP_DURATION : COUNT_UP_DEFAULT_DURATION}
-                end={TRANSACTIONS_PER_SECOND}
-                separator=","
-                start={0}
-                useEasing
-              />
-            </S.StatisticNumber>
-            <S.StatisticDescription>transactions per second</S.StatisticDescription>
-          </S.StatisticSection>
-          <S.StatisticDivider type={width > 992 ? 'vertical' : 'horizontal'} />
-          <S.StatisticSection>
-            <S.StatisticTitle>Latency</S.StatisticTitle>
-            <S.StatisticNumber>
-              <CountUp
-                decimals={3}
-                delay={COUNT_UP_DELAY}
-                duration={viewPortEntered ? COUNT_UP_DURATION : COUNT_UP_DEFAULT_DURATION}
-                end={LATENCY}
-                start={5}
-                useEasing
-              />
-            </S.StatisticNumber>
-            <S.StatisticDescription>second block time</S.StatisticDescription>
-          </S.StatisticSection>
-          <S.StatisticDivider type={width > 992 ? 'vertical' : 'horizontal'} />
-          <S.StatisticSection>
-            <S.StatisticTitle>Eco-friendly</S.StatisticTitle>
-            <S.StatisticTransactionDescription>
-              A fast and low carbon footprint blockchain.
-            </S.StatisticTransactionDescription>
-          </S.StatisticSection>
-        </S.StatisticsContainer>
-      </VisibilitySensor>
+      <S.StatisticsContainer>
+        <S.StatisticSection>
+          <S.StatisticTitle>Speed</S.StatisticTitle>
+          <S.StatisticNumber>
+            <CountUp
+              delay={COUNT_UP_DELAY}
+              duration={COUNT_UP_DURATION}
+              end={TRANSACTIONS_PER_SECOND}
+              separator=","
+              start={0}
+              useEasing
+            />
+          </S.StatisticNumber>
+          <S.StatisticDescription>transactions per second</S.StatisticDescription>
+        </S.StatisticSection>
+        <S.StatisticDivider type={width > 992 ? 'vertical' : 'horizontal'} />
+        <S.StatisticSection>
+          <S.StatisticTitle>Latency</S.StatisticTitle>
+          <S.StatisticNumber>
+            <CountUp
+              decimals={3}
+              delay={COUNT_UP_DELAY}
+              duration={COUNT_UP_DURATION}
+              end={LATENCY}
+              start={5}
+              useEasing
+            />
+          </S.StatisticNumber>
+          <S.StatisticDescription>second block time</S.StatisticDescription>
+        </S.StatisticSection>
+        <S.StatisticDivider type={width > 992 ? 'vertical' : 'horizontal'} />
+        <S.StatisticSection>
+          <S.StatisticTitle>Eco-friendly</S.StatisticTitle>
+          <S.StatisticTransactionDescription>
+            A fast and low carbon footprint blockchain.
+          </S.StatisticTransactionDescription>
+        </S.StatisticSection>
+      </S.StatisticsContainer>
       <S.Image alt="TNB Globe" src={GlobeImage} />
     </S.Container>
   );
